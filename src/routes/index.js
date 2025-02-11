d25ba8c9ddd9cd34a8870e9e27700ba617298367
Erikrussian <caohoanglinh203@gmail.com>
2025-02-11 20:49:57 +0700

    update create create order and create product, fix order model

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const loginRouter = require('./login')
 const userRouter = require('./user')
 const registerRouter = require('./register')
 const categoryRouter = require('./category')
+const orderRouter = require('./order')
 function route(app) {
     app.use('/', siteRouter)
     app.use('/login', loginRouter)
     app.use('/product', productRouter)
     app.use('/user', userRouter)
     app.use('/register', registerRouter)
-    app.use('/', categoryRouter)
+    app.use('/category', categoryRouter)
+    app.use('/order', orderRouter)
 }
 module.exports = route
--- src/routes/oder.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const router = express.Router();
-const productController = require('../app/controllers/productController');
-
-router.post('/oder/:userId/:productId', productController.createOrder);
-
-module.exports = router;
