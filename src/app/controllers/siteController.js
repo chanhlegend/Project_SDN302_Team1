b3eb7aaf096015857a237813d1f6fb64a69a3125
Tran Hoang Anh <thathatha2003@gmail.com>
2025-02-20 00:11:36 +0700

    Update UI Db

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,31 +1,36 @@
-const Product = require('../models/Product')
-const Category = require('../models/Category')
+const Product = require('../models/Product');
+const Category = require('../models/Category');
+const Image = require('../models/Image'); // Thêm mô hình Image
+const User = require('../models/User'); // Thêm mô hình User
 
 class siteController {
-    async index(req, res) {
-        const user = req.session.user;
-        const products = await Product.find().sort({ createdAt: -1 });
+  async index(req, res) {
+    const user = req.session.user;
+    const products = await Product.find().sort({ createdAt: -1 })
+      .populate('image')
+      .populate('sellerId'); // Sử dụng populate để truy xuất thông tin người bán
 
-        const laptopCategory = await Category.findOne({ name: 'Laptop' });
-        const laptops = laptopCategory ? await Product.find({ category: laptopCategory._id }).sort({ createdAt: -1 }) : [];
+    const laptopCategory = await Category.findOne({ name: 'Laptop' });
+    const laptops = laptopCategory ? await Product.find({ category: laptopCategory._id }).sort({ createdAt: -1 })
+      .populate('image')
+      .populate('sellerId') : [];
 
+    const phoneCategory = await Category.findOne({ name: 'Phone' });
+    const phones = phoneCategory ? await Product.find({ category: phoneCategory._id }).sort({ createdAt: -1 })
+      .populate('image')
+      .populate('sellerId') : [];
 
-        const phoneCategory = await Category.findOne({ name: 'Phone' });
-        const phones = phoneCategory ? await Product.find({ category: phoneCategory._id }).sort({ createdAt: -1 }) : [];
+    const categories = await Category.find().sort({ createdAt: -1 });
 
-        const categories = await Category.find().sort({ createdAt: -1 });
-
-        console.log('products', products);
-        res.render('home', {
-            user,
-            products,
-            laptops,
-            phones,
-            categories
-        });
-
-        // res.render('partials/footer');
-    }
+    console.log(products);
+    res.render('home', {
+      user,
+      products,
+      laptops,
+      phones,
+      categories
+    });
+  }
 }
 
 module.exports = new siteController();
