8caae03e5e10aa3fcda6c3f131a6962af493003d
Tran Hoang Anh <thathatha2003@gmail.com>
2025-02-19 20:27:52 +0700

    update db user

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const productController = require('../app/controllers/productController');
 
 router.get('/listProduct', productController.listProduct)
-router.get('/myStatics', sellerProductController.getMyProducts)
 router.post('/createProduct', productController.createProduct) 
 
 module.exports = router;
