ac47411e9dd63aba3cf259691fec26a4ae361e89
Tran Hoang Anh <thathatha2003@gmail.com>
2025-02-27 09:51:16 +0700

    fix conflix

--- src/routes/review.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ReviewController = require('../app/controllers/reviewController');
-
-router.post('/', ReviewController.reviewSeller);
-router.get('/reviews/:sellerId', ReviewController.getSellerReviews);
-
-module.exports = router;
