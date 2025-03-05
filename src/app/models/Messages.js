6c0da3a579fa3e7ae344504d5d023ea998f2b292
Tran Hoang Anh <thathatha2003@gmail.com>
2025-03-05 15:22:52 +0700

    update db

--- src/app/models/Messages.js
+++ src/app/models/Messages.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Messages = new Schema({
-    conversation_id: { type: String, required: true },
-    senderId: { type: String, required: true },
+    conversation_id: { type: Schema.Types.ObjectId, ref: 'Conversations' , required: true },
+    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     messageContent: { type: String, required: true },
 }, { timestamps: true });
 
